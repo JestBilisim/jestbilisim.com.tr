bbb7af08194ce8b763938d8cca29451c632ff1a2
merk <merkjs@aol.com>
2018-10-30 22:35:42 +0300

    updated readme

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Çok yakında -
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.facebook.com/jestbilisim"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Facebook
           </a>
         </header>
       </div>
